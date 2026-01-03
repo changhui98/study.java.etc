af37c0e4d17f208469d67ace5dab575e618049a8
changhui98 <net_0102@naver.com>
2026-01-03 13:52:50 +0900

    Study : 캐스팅의 종류

---
+++ src/poly/basic/CastingMain3.java
@@ -0,0 +1,18 @@
+package poly.basic;
+
+public class CastingMain3 {
+
+    public static void main(String[] args) {
+
+        // upcasting vs downcasting
+        Child child = new Child();
+        Parent parent1 = (Parent) child; // 업 캐스팅은 생략 가능, 생략 권장
+        Parent parent2 = child; // 업 캐스팅 생략
+
+        parent1.parentMethod();
+        parent2.parentMethod();
+
+
+
+    }
+}
