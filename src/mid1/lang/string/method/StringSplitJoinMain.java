51faa968b0f0d4eaf258c331c0055666faaf370d
changhui98 <net_0102@naver.com>
2026-01-21 01:08:34 +0900

    Study : String 클래스 주요 메서드 2

---
+++ src/mid1/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,35 @@
+package mid1.lang.string.method;
+
+public class StringSplitJoinMain {
+
+    public static void main(String[] args) {
+
+        String str = "Apple,Banana,Orange";
+
+        // split()
+        String[] split = str.split(",");
+        for (String s : split) {
+            System.out.println(s);
+        }
+        String joinStr = "";
+
+        for (int i = 0; i < split.length; i++) {
+            String string = split[i];
+            joinStr += string;
+            if (i != split.length -1) {
+                joinStr += ",";
+            }
+        }
+        System.out.println(joinStr);
+
+        // join()
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println(joinedStr);
+
+        // 문자열 배열 연결
+        String result = String.join("-", split);
+        System.out.println(result);
+
+    }
+
+}
---
+++ src/mid1/lang/string/method/StringUtilsMain1.java
@@ -0,0 +1,34 @@
+package mid1.lang.string.method;
+
+public class StringUtilsMain1 {
+
+    public static void main(String[] args) {
+
+        int num = 100;
+        boolean bool = true;
+        Object obj = new Object();
+        String str = "Hello, Java!";
+
+        // valueOf 메서드
+        String numString = String.valueOf(num);
+        System.out.println("숫자의 문자열 값 : " + numString);
+
+        String boolString = String.valueOf(bool);
+        System.out.println("불리언의 문자열 값 : " + boolString);
+
+        String objectString = String.valueOf(obj);
+        System.out.println("객체의 문자열 값 : " + objectString);
+
+        // 문자 + x -> 문자
+        String numString2 = "" + num;
+        System.out.println("빈 문자열 + num : " + numString2);
+
+        // toCharArray 메서드
+        char[] strCharArray = str.toCharArray();
+        System.out.println("문자열을 문자 배열로 반환 : " + strCharArray);
+        for (char c : strCharArray) {
+            System.out.print(c);
+        }
+    }
+
+}
