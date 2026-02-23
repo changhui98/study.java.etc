b06ded49f0d82cb7a64da73cb045f91a7c483986
changhui98 <net_0102@naver.com>
2026-02-23 21:12:10 +0900

    Study : 정적 중첩 클래스의 활용

---
+++ src/mid1/nested/nested/ex1/NetworkMessage.java
@@ -0,0 +1,15 @@
+package mid1.nested.nested.ex1;
+
+// Network 객체 안에서만 사용
+public class NetworkMessage {
+
+    private String content;
+
+    public NetworkMessage(String content) {
+        this.content = content;
+    }
+
+    public void pirnt() {
+        System.out.println(content);
+    }
+}
