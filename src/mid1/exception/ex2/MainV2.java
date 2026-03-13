7131a467d4f6cd672063c6eddc13cf77a9c3e437
changhui98 <net_0102@naver.com>
2026-03-13 22:57:47 +0900

    Study : 예외 처리 도입 1

---
+++ src/mid1/exception/ex2/MainV2.java
@@ -0,0 +1,26 @@
+package mid1.exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+
+//        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 문자 : ");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("로그램을 정상 종료합니다.");
+    }
+
+}
