a5a81020357d7988da0bd7e7fb9a0af20953c6e6
changhui98 <net_0102@naver.com>
2026-03-21 21:34:46 +0900

    Study : 예외 계층 시작

---
+++ src/mid1/exception/ex3/MainV3.java
@@ -0,0 +1,31 @@
+package mid1.exception.ex3;
+
+import java.util.Scanner;
+
+public class MainV3 {
+
+    public static void main(String[] args)  {
+
+//        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+//        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+//        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+//        NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+//        NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+//        NetworkServiceV2_5 networkService = new NetworkServiceV2_5();
+        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 문자 : ");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("로그램을 정상 종료합니다.");
+    }
+
+}
